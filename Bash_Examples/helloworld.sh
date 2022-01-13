aabb1b511d44b1d53b077616e64e33c20e55f1a6
smit-einfochips <smit.majithia@einfochips.com>
2022-01-13 15:02:27 +0530

    change script

---
+++ Bash_Examples/helloworld.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -x
+
+echo "Hello World from Bash!"
+
+
---
+++ script_WP.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -x
+ 
+pwd
+ 
+echo "Compiling Hello World C program"
+ 
+cd C_Examples
+ 
+ls
+gcc -Wall helloworld.c -o helloworld
+ 
+if [ $? -ne 0 ]; then
+ 
+echo "Hello world C program compilation failed"
+else
+ls -ltr
+fi
+
+## for python compilation
+echo "Compiling Hello World Python program"
+cd ../Python_Examples
+ 
+python3 helloworld.py
+ 
+if [ $? -ne 0 ]; then
+ 
+echo "Hello world Python program failed"
+fi
+
+
+echo "Compiling Hello World Bash program"
+cd ../Bash_Examples
+ 
+bash helloworld.sh
+if [ $? -ne 0 ]; then
+ 
+echo "Hello world Bash script execution failed"
+fi
